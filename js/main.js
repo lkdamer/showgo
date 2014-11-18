07aada4011a6c41a55850781e84f8acfef6b2d10
Mixolidia Gautreaux <mixolidia@gmail.com>
2014-11-17 20:32:31 -0800

    Add form_test.html to test Mail Chimp submission.

---
+++ js/main.js
@@ -0,0 +1,31 @@
+$(document).ready( function () {
+    // I only have one form on the page but you can be more specific if need be.
+    var $form = $('form');
+
+    if ( $form.length > 0 ) {
+        $('form input[type="submit"]').bind('click', function ( event ) {
+            if ( event ) event.preventDefault();
+            // validate_input() is a validation function I wrote, you'll have to substitute this with your own.
+             register($form);
+        });
+    }
+});
+
+function register($form) {
+    $.ajax({
+        type: $form.attr('method'),
+        url: $form.attr('action'),
+        data: $form.serialize(),
+        cache       : false,
+        dataType    : 'json',
+        contentType: "application/json; charset=utf-8",
+        error       : function(err) { alert("Could not connect to the registration server. Please try again later."); },
+        success     : function(data) {
+            if (data.result != "success") {
+            console.log(data)
+             } else {
+                console.log(data) 
+            }
+        }
+    });
+  }
